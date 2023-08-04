d83b6e0514eb3c34b31800f9aab404a888ee6331
Lee <leejavaa@gmail.com>
2023-08-04 10:37:15 +0200

    created scraper

---
+++ sandbox/scraper/api_scraper.py
@@ -0,0 +1,8 @@
+def main():
+    pass
+
+    # Get API data from youtube API for every video by steve brunton
+    # Export that API data to csv
+
+if __name__ == '__main__':
+    main()
---
+++ sandbox/scraper/video_downloader.py
@@ -0,0 +1,9 @@
+def main():
+    pass
+
+    # get video data
+    # download video
+    # output video to folder (ignored on git) with formatted file na
+
+if __name__ == "__main__":
+    main()
